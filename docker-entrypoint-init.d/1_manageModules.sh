bf7fc696de870bc0f36d9331cfeac7ac76d22fee
gbitdev <m.gontarek@gbitweb.pl>
2020-03-24 10:17:17 +0100

    Add installing and enabling modules

--- docker-entrypoint-init.d/1_manageModules.sh
+++ docker-entrypoint-init.d/1_manageModules.sh
@@ -4,3 +4,17 @@ gosu bitnami $WORKDIR/bin/magerun2 module:disable \
     $MODULES_TO_DISABLE \
     $(magerun2 dev:module:list --format=csv | grep -e Adobe -e Amazon -e Temando -e Klarna -e Yotpo | awk -F ',' '{print $1}' | tr '\n' ' ') && \
 echo "Modules disabled"
+
+gosu bitnami /opt/bitnami/php/bin/composer require --update-no-dev \
+    stripe/module-payments
+    $MODULES_TO_INSTALL && \
+echo "Modules Installed"
+
+if [ -z "$MODULES_TO_ENABLE" ]
+then
+    echo "Nothing to enable"
+else
+    gosu bitnami $WORKDIR/bin/magerun2 module:enable \
+        $MODULES_TO_ENABLE && \
+    echo "Modules Enabled"
+end
