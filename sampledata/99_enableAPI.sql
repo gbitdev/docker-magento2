d6a7707a9d095a0e693c399833720c1238af50ae
gbitdev <m.gontarek@gbitweb.pl>
2020-03-24 12:09:18 +0100

    Enable instagration via SQL file

---
+++ sampledata/99_enableAPI.sql
@@ -0,0 +1,3 @@
+INSERT INTO `oauth_consumer` VALUES ('1','2020-03-24 10:39:37','0000-00-00 00:00:00','Integration1','w0ojrk9bu7efwooh5b34rvxdxav8yd4e','tvpn76yh77n3xm2qznlnoyobw2dynnjp',NULL,'');
+INSERT INTO `oauth_token` VALUES ('1','1',NULL,NULL,'access','j3ddvqj6lhd8uwm9y4fc2hgmydh82fh0','zn10jms5ht4qw2oybd81pryw9lb8h6kz','1ev1z2chu9lxa78gk402p54et9b064xj','oob',0,0,'1','2020-03-24 10:40:32');
+INSERT INTO `integration` VALUES ('1','storefront-api','',NULL,1,'1','2020-03-24 10:39:37','2020-03-24 10:40:32',0,NULL);
