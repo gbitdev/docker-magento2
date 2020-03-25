f19068b7ce5b0e29cf0155543f42f3df176d0867
gbitdev <m.gontarek@gbitweb.pl>
2020-03-25 09:03:04 +0100

    Typo - cache:clear -> cache:clean

--- docker-entrypoint-init.d/1_setCustomTheme.sh
+++ docker-entrypoint-init.d/1_setCustomTheme.sh
@@ -5,7 +5,7 @@
 THEME_ID=$(gosu bitnami $WORKDIR/bin/magerun2 dev:theme:list --format=csv | grep "${THEME}" | cut -d, -f1); test -n "${THEME_ID}" && \
 gosu bitnami $WORKDIR/bin/magerun2 config:store:set design/theme/theme_id "${THEME_ID}"
 gosu bitnami $WORKDIR/bin/magerun2 cache:flush && \
-gosu bitnami $WORKDIR/bin/magerun2 cache:clear 
+gosu bitnami $WORKDIR/bin/magerun2 cache:clean 
 if [ -z $THEME_ID ]
 then
     warn "THEME variable not setted"
