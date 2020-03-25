c2ecc4dd1bf1dfa80a82a613b87a747c83362b56
gbitdev <m.gontarek@gbitweb.pl>
2020-03-25 11:59:15 +0100

    Remove concurency from theme files generation due errors

--- rootfs/docker-entrypoint-restore.d/2_regenerateFiles.sh
+++ rootfs/docker-entrypoint-restore.d/2_regenerateFiles.sh
@@ -9,9 +9,9 @@ gosu bitnami $WORKDIR/bin/magento setup:di:compile  && \
 info "Upgrade & compile complete"
 
 gosu bitnami gulp -f tools/gulpfile.esm.js setup && info "Gulp stup complete"
-gosu bitnami gulp -f tools/gulpfile.esm.js babel && info "Theme JS files compiled" &
-gosu bitnami gulp -f tools/gulpfile.esm.js styles && info "Theme CSS files compiled" &
-gosu bitnami gulp -f tools/gulpfile.esm.js svg && info "Theme SVG files compiled" &
+gosu bitnami gulp -f tools/gulpfile.esm.js babel && info "Theme JS files compiled"
+gosu bitnami gulp -f tools/gulpfile.esm.js styles && info "Theme CSS files compiled"
+gosu bitnami gulp -f tools/gulpfile.esm.js svg && info "Theme SVG files compiled"
 gosu bitnami gulp -f tools/gulpfile.esm.js babel --prod && info "JS minified" 
 
 gosu bitnami $WORKDIR/bin/magerun2 setup:static-content:deploy -t $THEME -t 'Magento/backend' -f pl_PL en_US && \
