21374e951ae9c89e654bdab2facd2848588598af
gbitdev <m.gontarek@gbitweb.pl>
2020-03-25 08:17:04 +0100

    Typo - fix lack of /opt/bitnami/base/functions in file

--- docker-entrypoint-restore.d/1_fixBannerSliderModule.sh
+++ docker-entrypoint-restore.d/1_fixBannerSliderModule.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 
+. /opt/bitnami/base/functions
+
 gosu bitnami sed -i 's/$locations =/if (empty($slider->getLocation())) continue;\n$locations =/' vendor/mageplaza/module-banner-slider/Observer/AddBlock.php && \
 info "MagePlaza Banner Slider fixed"
