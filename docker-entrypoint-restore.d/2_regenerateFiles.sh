b06c79b4dc8ed1213f45ec5ebd6ba578dabeb6c8
gbitdev <m.gontarek@gbitweb.pl>
2020-03-25 07:22:50 +0100

    Fix permissions and change from echo to helper function info

--- docker-entrypoint-restore.d/2_regenerateFiles.sh
+++ docker-entrypoint-restore.d/2_regenerateFiles.sh
@@ -8,8 +8,11 @@ gosu bitnami $WORKDIR/bin/magento setup:upgrade  && \
 gosu bitnami $WORKDIR/bin/magento setup:di:compile  && \
 info "Upgrade & compile complete"
 
-gosu bitnami gulp -f tools/gulpfile.esm.js setup && gulp -f tools/gulpfile.esm.js babel styles svg && info "Theme files compiled" 
-gosu bitnami gulp -f tools/gulpfile.esm.js setup && gulp -f tools/gulpfile.esm.js babel --prod && info "JS minified" 
+gosu bitnami gulp -f tools/gulpfile.esm.js setup && info "Gulp stup complete"
+gosu bitnami gulp -f tools/gulpfile.esm.js babel && info "Theme JS files compiled" &
+gosu bitnami gulp -f tools/gulpfile.esm.js styles && info "Theme CSS files compiled" &
+gosu bitnami gulp -f tools/gulpfile.esm.js svg && info "Theme SVG files compiled" &
+gosu bitnami gulp -f tools/gulpfile.esm.js babel --prod && info "JS minified" 
 
 gosu bitnami $WORKDIR/bin/magerun2 setup:static-content:deploy -t $THEME -t 'Magento/backend' -f pl_PL en_US && \
-info "Static deploy complete"
+info "Static files deploy complete"
