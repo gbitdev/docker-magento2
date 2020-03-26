6babeac5046d30a08d1c3d9a3446ae0ba63a8134
gbitdev <m.gontarek@gbitweb.pl>
2020-03-26 07:33:44 +0100

    Enable webP bineries

---
+++ rootfs/docker-entrypoint-restore.d/1_webPSupport.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+
+chmod +x /opt/bitnami/magento/htdocs/vendor/rosell-dk/webp-convert/src/Convert/Converters/Binaries/*
+ln -s /opt/bitnami/magento/htdocs/vendor/rosell-dk/webp-convert/src/Convert/Converters/Binaries/* /usr/local/bin
